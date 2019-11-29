b1e6e2efd63265b718931cc59db047860ff94931
Nmusavi <navid.musavii@icloud.com>
2019-11-29 16:36:08 +0100

    Updates

---
+++ precache-manifest.0f7457e981aa5b85e8d7fe9bcc8ba6bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beff1bb9010155ea30de6fe10aacc0f9",
+    "url": "/AbschlussPrijekt.git/index.html"
+  },
+  {
+    "revision": "e34260a10e3b935387a9",
+    "url": "/AbschlussPrijekt.git/static/css/main.2f7bcea2.chunk.css"
+  },
+  {
+    "revision": "2fa97e17c841c3e9d7f3",
+    "url": "/AbschlussPrijekt.git/static/js/2.c7557b77.chunk.js"
+  },
+  {
+    "revision": "e34260a10e3b935387a9",
+    "url": "/AbschlussPrijekt.git/static/js/main.e5012d76.chunk.js"
+  },
+  {
+    "revision": "94f7252a140510607d90",
+    "url": "/AbschlussPrijekt.git/static/js/runtime-main.ac65222c.js"
+  },
+  {
+    "revision": "d157c2d667adc8e400cff77eaaf35ec8",
+    "url": "/AbschlussPrijekt.git/static/media/Login_Background.d157c2d6.png"
+  },
+  {
+    "revision": "759f51bb1f2adeb90da13ee15db564c0",
+    "url": "/AbschlussPrijekt.git/static/media/Main_Background.759f51bb.png"
+  }
+]);
